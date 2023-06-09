feadb39441cbe0e10c93a72cbbf933f2a421e128
zt <2916461184@qq.com>
2023-06-09 15:54:21 +0800

    系统管理模块基础环境

---
+++ service/service-sys/src/main/java/com/atguigu/ssyx/ServiceSysApplication.java
@@ -0,0 +1,22 @@
+package com.atguigu.ssyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: guigu-ssyx-parent
+ * @BelongsPackage: com.atguigu.ssyx
+ * @Author: zt
+ * @CreateTime: 2023-06-09  15:33
+ * @Description:
+ */
+
+@SpringBootApplication
+public class ServiceSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSysApplication.class, args);
+    }
+
+
+}
