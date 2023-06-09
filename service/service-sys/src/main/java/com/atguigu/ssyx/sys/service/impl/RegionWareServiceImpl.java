8d35e1eb7c4777cd3fc601924ab472c45055ab58
zt <2916461184@qq.com>
2023-06-09 15:53:52 +0800

    自动生成

---
+++ service/service-sys/src/main/java/com/atguigu/ssyx/sys/service/impl/RegionWareServiceImpl.java
@@ -0,0 +1,20 @@
+package com.atguigu.ssyx.sys.service.impl;
+
+import com.atguigu.ssyx.model.sys.RegionWare;
+import com.atguigu.ssyx.sys.mapper.RegionWareMapper;
+import com.atguigu.ssyx.sys.service.RegionWareService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 城市仓库关联表 服务实现类
+ * </p>
+ *
+ * @author zt
+ * @since 2023-06-09
+ */
+@Service
+public class RegionWareServiceImpl extends ServiceImpl<RegionWareMapper, RegionWare> implements RegionWareService {
+
+}
